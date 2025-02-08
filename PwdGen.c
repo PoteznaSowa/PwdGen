0cb7b858988a3a6f35083c7c762e033a66b3f09a
MightyFixie <maximusf625@gmail.com>
2025-02-09 01:58:25 +0200

    Added shuffling

--- PwdGen.c
+++ PwdGen.c
@@ -79,6 +79,18 @@ static int PullModulo(unsigned long long* n, int m, int o) {
 	return r;
 }
 
+/*
+* Swaps two array elements.
+* @param	s	Array
+* @param	i1	First element index
+* @param	i2	Second element index
+*/
+static void Swap(char s[], int i1, int i2) {
+	char c = s[i1];
+	s[i1] = s[i2];
+	s[i2] = c;
+}
+
 int
 #ifdef _WIN32
 __cdecl
@@ -86,41 +98,46 @@ __cdecl
 main() {
 	/*
 	* Generate and show random text which consists of:
-	* - 20 ASCII characters;
+	* - 16 ASCII characters;
 	* - 6 decimal digits;
 	* - 32 hexadecimal digits.
 	*/
 
 	unsigned long long rnum;
 	unsigned long long rnum2[2];
-	char pwd[20];
+	char pwd[16];
 
 	GetRandom(rnum2, sizeof(rnum2));
 
 	rnum = rnum2[0];
-
-	for (int i = 0; i < 7; i++) {
-		pwd[i] = PullModulo(&rnum, 94, '!');
-	}
 
 	/*
 	* It is guaranteed that the password contains at least one of these:
 	* - digit;
 	* - upper letter;
 	* - lower letter;
-	* - punctuation character.
+	* - punctuation symbol.
 	*/
-	pwd[7] = PullModulo(&rnum, 10, '0');
-	pwd[8] = PullModulo(&rnum, 26, 'A');
-	pwd[9] = PullModulo(&rnum, 26, 'a');
-	pwd[10] = PullModulo(&rnum, 15, '!');
-
-	rnum = rnum2[1];
-
-	for (int i = 11; i < 20; i++) {
+	const char puncts[] = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+	pwd[0] = PullModulo(&rnum, 10, '0');
+	pwd[1] = PullModulo(&rnum, 26, 'A');
+	pwd[2] = PullModulo(&rnum, 26, 'a');
+	pwd[3] = puncts[PullModulo(&rnum, 32, 0)];
+	
+	for (int i = 4; i < 10; i++) {
 		pwd[i] = PullModulo(&rnum, 94, '!');
 	}
 
-	printf("%.20s %.6d %016llx%016llx\n", pwd, (int)(rnum2[0] % 1000000), rnum2[0], rnum2[1]);
+	rnum = rnum2[1];
+
+	for (int i = 10; i < 16; i++) {
+		pwd[i] = PullModulo(&rnum, 94, '!');
+	}
+	Swap(pwd, 0, PullModulo(&rnum, 16, 0));
+	Swap(pwd, 1, PullModulo(&rnum, 15, 0));
+	Swap(pwd, 2, PullModulo(&rnum, 14, 0));
+	Swap(pwd, 3, PullModulo(&rnum, 13, 0));
+
+	printf("%.16s %.6d %016llx%016llx\n", pwd, (int)(rnum2[0] % 1000000), rnum2[0], rnum2[1]);
 	return 0;
 }
